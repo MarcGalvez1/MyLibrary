14a6ceafce7562a16d845862ccad1fbdc7696eb8
MarcGalvez1 <marc.galvez83@gmail.com>
2023-11-08 13:08:43 -0500

    making the js object array

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
-const myLibrary = [];
-
-function Book() {
+function Book(title, author, numPages) {
   // the constructor...
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+
+
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
   // do stuff here
+  myLibrary.push(book);
+  for (const book of myLibrary) {
+    console.log(book);
+  }
+
+}
+
+function AddBook() {
+  let bookTtl = document.getElementById("Title").value;
+  let bookAuthor = document.getElementById("Author").value;
+  let numPages = document.getElementById("NumPages").value;
+
+  const newBook = new Book(bookTtl, bookAuthor, numPages);
+  
+  addBookToLibrary(newBook);
+}
+
+
+// Variables
+const myLibrary = [];
+const addBookForm = document.getElementById("addBookForm");
+
+
+addBookForm.onsubmit = (event) => {
+  // Processes file submission
+  event.preventDefault();
+  AddBook();
 }
