2e74cbeea1998c963a4d6691319f81587eb9613c
MarcGalvez1 <marc.galvez83@gmail.com>
2023-11-08 21:31:20 -0500

    creating new cards

--- main.js
+++ main.js
@@ -3,17 +3,44 @@ function Book(title, author, numPages) {
     this.title = title;
     this.author = author;
     this.numPages = numPages;
-
+    this.cardContainer = document.getElementById("content-container");
 
 }
 
+Book.prototype.createCard = function() {
+  // Creates cards to display the books
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardTtl = document.createElement('h5');
+  cardTtl.classList.add('card-title');
+  cardTtl.textContent = this.title;
+
+  const cardAuthor = document.createElement('h6');
+  cardAuthor.classList.add('card-subtitle');
+  cardAuthor.textContent = this.author;
+
+  const cardPages = document.createElement('p');
+  cardPages.classList.add("card-text");
+  cardPages.textContent = "Number of Pages: " + this.numPages; 
+
+  card.appendChild(cardTtl);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPages);
+  this.cardContainer.appendChild(card);
+}
+
+
+
 function addBookToLibrary(book) {
   // do stuff here
-  myLibrary.push(book);
-  for (const book of myLibrary) {
-    console.log(book);
+  
+  if (!isBookInArray(myLibrary, book)) { 
+    myLibrary.push(book);
+    book.createCard();
+  } else {
+    window.alert("This book already exists, please enter a new book.")
   }
-
 }
 
 function AddBook() {
@@ -26,6 +53,13 @@ function AddBook() {
   addBookToLibrary(newBook);
 }
 
+function isBookInArray(library, newBook) {
+  return library.some((book) => {
+    return book.name === newBook.name && book.author === newBook.author && book.numPages === newBook.numPages;
+  });
+}
+
+
 
 // Variables
 const myLibrary = [];
@@ -36,4 +70,11 @@ addBookForm.onsubmit = (event) => {
   // Processes file submission
   event.preventDefault();
   AddBook();
+  for (const book of myLibrary) {
+    console.log(book);
+  }
 }
+
+
+
+
